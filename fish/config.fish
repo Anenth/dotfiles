d0cb806f2d54f56a655591ffe2bf6d558775cdb8
Anenth <anenthvishnu@gmail.com>
2020-08-13 21:21:23 +0530

    Update the git prompt colors

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,45 @@ fish_vi_key_bindings
 
 source ~/.config/fish/path.fish
 source ~/.config/fish/aliases.fish
+
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showupstream auto
+set __fish_git_prompt_color_branch 'white'
+set __fish_git_prompt_color_dirtystate 'white' --bold
+set __fish_git_prompt_bg_dirtystate 'white' --bold
+set __fish_git_prompt_color_dirtystate_behind white --bold
+
+# set __fish_git_prompt_color_upstream_ahead ffb90f
+# set __fish_git_prompt_color_upstream_behind blue
+
+
+
+
+# set __fish_git_prompt_color_branch magenta --bold
+# set __fish_git_prompt_color_dirtystate white
+# set __fish_git_prompt_color_invalidstate red
+# set __fish_git_prompt_color_merging yellow
+# set __fish_git_prompt_color_stagedstate yellow
+# set __fish_git_prompt_color_upstream_ahead green
+# set __fish_git_prompt_color_upstream_behind red
+
+
+# # Icons
+set __fish_git_prompt_char_cleanstate '👍 '
+set __fish_git_prompt_char_conflictedstate '⚠️ '
+set __fish_git_prompt_char_dirtystate '💩  '
+set __fish_git_prompt_char_invalidstate '🤮 '
+set __fish_git_prompt_char_stagedstate '🚥 '
+set __fish_git_prompt_char_stashstate '📦 '
+set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_untrackedfiles '🔍 '
+set __fish_git_prompt_char_upstream_ahead ' ☝ ️'
+set __fish_git_prompt_char_upstream_behind '👇 '
+set __fish_git_prompt_char_upstream_diverged '🚧 '
+set __fish_git_prompt_char_upstream_equal '💯 ' 
+
+
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_dirtystate 'red'
+set __fish_git_prompt_color_upstream_behind blue
+
