16b485f3ab4c4093ab61fe1fe46abac0fdb1e466
Anenth <anenthvishnu@gmail.com>
2021-04-14 18:31:47 +0530

    Update config

--- cron/daily-nighttime.sh
+++ cron/daily-nighttime.sh
@@ -3,7 +3,5 @@
 # update devtools standalone repo
 cd "$HOME/code/npm-publish-devtools-frontend" && ./update-github-mirror.sh && ./publish-devtools-package-to-npm.sh
 
-# prepare the locate database
-LC_ALL=C /Users/paulirish/.homebrew/bin/gupdatedb --prunepaths="/tmp /var/tmp /.Spotlight-V100 /.fseventsd /Volumes/MobileBackups /Volumes/Volume /.MobileBackups"
 
 
--- cron/twice-daily-daytime.sh
+++ cron/twice-daily-daytime.sh
@@ -3,8 +3,6 @@
 # `crontab -l` sez this runs TWICE daily.. at 2pm and 8pm
 
 
-PATH=/Users/paulirish/.homebrew/bin:/Users/paulirish/.homebrew/sbin:/Users/paulirish/code/depot_tools:$PATH
-
 
 cd "$HOME/code/npm-publish-devtools-frontend" && ./update-github-mirror.sh
 
